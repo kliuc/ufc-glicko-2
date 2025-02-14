4a022bc233611bf81f5b9d4fa5921f0373f6871d
kliuc <115437942+kliuc@users.noreply.github.com>
2025-02-14 01:26:14 -0800

    Create ufcofficial scraping module

---
+++ scraping/ufcofficial.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_fighters(status='active'):
+    if status == 'active':
+        url = 'https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23&page='
+    if status == 'all':
+        url = 'https://www.ufc.com/athletes/all?page='
+
+    all_fighters = []
+    page_num = 1
+    while page_num <= 1000:
+        response = requests.get(url + str(page_num))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        if soup.find('div', class_='ufc-search-no-results'):
+            break
+        fighter_cards = soup.find_all('li', class_='l-flex__item')
+        fighters = [{'name': fighter.find('span', class_='c-listing-athlete__name').text.strip(),
+                     'weight_class': fighter.find('span', class_='c-listing-athlete__title').text.strip(),
+                     'record': fighter.find('span', class_='c-listing-athlete__record').text.strip()}
+                     for fighter in fighter_cards
+                     if fighter.find('span', class_='c-listing-athlete__name')]
+        all_fighters.extend(fighters)
+        page_num += 1
+
+    return all_fighters
+
+def get_ranking(weight_class):
+    response = requests.get('https://www.ufc.com/rankings')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    rankings = soup.find_all('div', class_='view-grouping')
+    ranking = next(div for div in rankings
+                   if weight_class.lower() == div.find('h4').text.replace('Top Rank', '').strip().lower())
+    return [row.find_all('td')[1].text.strip() for row in ranking.find_all('tr')]
--- scraping/ufcstats.py
+++ scraping/ufcstats.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from datetime import datetime
 
 
@@ -20,21 +19,24 @@ def get_completed_events(latest_n=None):
         response = requests.get(href)
         soup = BeautifulSoup(response.text, 'html.parser')
         fights_table = soup.find('tbody')
-        for fight in fights_table.find_all('tr'):
-            columns = fight.find_all('td')
-            fight_data = {'event': name,
-                          'date': date,
-                          'fighter': columns[1].find_all('p')[0].text.strip(),
-                          'opposition': columns[1].find_all('p')[1].text.strip(),
-                          'weight_class': columns[6].text.strip(),
-                          'outcome': columns[0].find('p').text.strip(),
-                          'method': columns[7].find('p').text.strip()}
-            all_fights.append(fight_data)
+        fights = [{'event': name,
+                   'date': date,
+                   'fighter': columns[1].find_all('p')[0].text.strip(),
+                   'opposition': columns[1].find_all('p')[1].text.strip(),
+                   'weight_class': columns[6].text.strip(),
+                   'outcome': columns[0].find('p').text.strip(),
+                   'method': columns[7].find('p').text.strip()}
+                   for fight in fights_table.find_all('tr')
+                   for columns in [fight.find_all('td')]]
+        all_fights.extend(fights)
 
     return all_fights
 
 
 if __name__ == '__main__':
 
-    fights = pd.DataFrame(get_completed_events())
-    print(fights.head(10))
+    import pandas as pd
+    
+    fights = pd.DataFrame(get_completed_events(latest_n=5))
+    print(fights.head())
+    print(fights['event'].unique())
