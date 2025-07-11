0fd6b87b0e7d8a0852418afe0881ed42eefb1180
Kevin Liu <kliuc@ucla.edu>
2025-07-11 10:51:27 -0700

    Add matchup matrix calculation

--- rating/glicko2.py
+++ rating/glicko2.py
@@ -1,33 +1,34 @@
 import numpy as np
 from scipy.stats import norm
+import pandas as pd
+
 
 
 class Player:
+
     def __init__(self, rating=1500, rd=350, volatility=0.06, tau=0.5):
         self.rating = rating
         self._rd = rd
         self._volatility = volatility
         self._tau = tau
 
+
     def get_rating(self):
         return self.rating
     
+
     def get_rating_interval(self, confidence=0.95):
         z = norm.ppf(1 - (1-confidence) / 2)
         margin = z * self._rd
         return (float(self.rating - margin), float(self.rating + margin))
-
-    def p_a_beats_b(player_a, player_b):
-        rating_a, rating_b = player_a.rating, player_b.rating
-        phi_a, phi_b = player_a._phi(), player_b._phi()
-        g = 1 / np.sqrt(1 + 3*(phi_a**2 + phi_b**2) / np.pi**2)
-        return 1 / (1 + 10**(-g * (rating_a - rating_b) / 400))
     
-    def p_win(self, opponent):
+
+    def p_beats(self, opponent):
         rating, phi = self.rating, self._phi()
         rating_opp, phi_opp = opponent.rating, opponent._phi()
         g = 1 / np.sqrt(1 + 3*(phi**2 + phi_opp**2) / np.pi**2)
         return 1 / (1 + 10**(-g * (rating - rating_opp) / 400))
+
 
     def update_rating(self, opponents, scores):
         if len(opponents) == 0:
@@ -43,9 +44,11 @@ class Player:
             self.rating =  173.7178 * mu + 1500
             self._rd =  173.7178 * phi
 
+
     def did_not_compete(self):
         phi = self._new_rd()
         self._rd = 173.7178 * phi
+
 
     def _mu(self):
         return (self.rating - 1500) / 173.7178
@@ -108,3 +111,74 @@ class Player:
     
     def _new_rd(self):
         return np.sqrt(self._phi()**2 + self._volatility**2)
+
+
+
+class PlayerManager(dict):
+
+    def __init__(self, ids=[], players=None, volatility=0.06, tau=0.5):
+        self._volatility = volatility
+        self._tau = tau
+        super().__init__({})
+        self.add_players(ids, players)
+
+
+    def add_players(self, ids, players=None):
+        if players is None:
+            super().update({id: Player(volatility=self._volatility, tau=self._tau)
+                            for id in ids if id not in self})
+        else:
+            if len(ids) != len(players):
+                raise Exception('`ids` and `players` must be the same length.')
+            super().update({id: player 
+                            for id, player in zip(ids, players) if id not in self})
+
+
+    def update_players(self, player_ids, opponent_ids, scores):
+        # will update players and opponents corresponding to player_ids and opponent_ids
+        competitor_ids = set(player_ids + opponent_ids)
+        self.add_players(competitor_ids)
+        competitors_copy = {id: self[id] for id in competitor_ids}
+        for id, player in self.items():
+            if id not in competitor_ids:
+                player.did_not_compete()
+            else:
+                subset = [(opponent_id, score)
+                          for player_id, opponent_id, score in zip(player_ids, opponent_ids, scores)
+                          if player_id == id]
+                opponents_subset, scores_subset = list(zip(*subset))
+                player.update_rating(opponents_subset, scores_subset)
+
+
+    def p_a_beats_b(self, id_a, id_b):
+        player_a, player_b = self[id_a], self[id_b]
+
+        rating_a, rating_b = player_a.rating, player_b.rating
+        phi_a, phi_b = player_a._phi(), player_b._phi()
+
+        g = 1 / np.sqrt(1 + 3*(phi_a**2 + phi_b**2) / np.pi**2)
+        return 1 / (1 + 10**(-g * (rating_a - rating_b) / 400))
+
+
+    def get_matchup_matrix(self, ids=None):
+        if ids is None:
+            ids = self.keys()
+
+        ratings = np.array([self[id].rating for id in ids])
+        phis2 = np.array([self[id]._phi() for id in ids])**2
+
+        g = 1 / np.sqrt(1 + 3*(phis2[:, None] + phis2) / np.pi**2)
+        matchup_matrix = 1 / (1 + 10**(-g * (ratings[:, None] - ratings) / 400))
+
+        return pd.DataFrame(matchup_matrix, index=ids, columns=ids)
+
+
+
+if __name__ == '__main__':
+
+    player_a = Player(rating=1800)
+    player_b = Player(rating=1500, rd=500)
+    player_c = Player(rating=1300)
+    player_d = Player(rating=2100)
+    manager = PlayerManager(ids=['jon', 'dan', 'matt', 'tom'], players=[player_a, player_b, player_c, player_d])
+    print(manager.get_matchup_matrix().loc['tom'])
--- rating/glicko2_ufc.py
+++ rating/glicko2_ufc.py
@@ -1,24 +1,29 @@
-from rating.glicko2 import Player
+from rating.glicko2 import Player, PlayerManager
 import pandas as pd
 
 
+
 class Fighter(Player):
+
     # this is how scoring will be determined based on fight outcome and method
     SCORING_DICT = {'win': {'KO/TKO': 1, 'SUB': 1, 'U-DEC': 1, 'M-DEC': 0.85, 'S-DEC': 0.7, 'DQ': 0.55},
                    'loss': {'KO/TKO': 0, 'SUB': 0, 'U-DEC': 0, 'M-DEC': 0.15, 'S-DEC': 0.3, 'DQ': 0.45},
                    'draw': 0.5}
     
+    @staticmethod
+    def get_scores(outcomes, methods):
+        return [Fighter.SCORING_DICT[outcome][method] if outcome != 'draw'
+                else Fighter.SCORING_DICT['draw']
+                for outcome, method in zip(outcomes, methods)]
+    
+
     def __init__(self, weight_class=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.weight_class = weight_class
         self.peak_rating = 0
         self.streak = self.best_streak = 0
         self.history = []
-    
-    def get_scores(outcomes, methods):
-        return [Fighter.SCORING_DICT[outcome][method] if outcome != 'draw'
-                else Fighter.SCORING_DICT['draw']
-                for outcome, method in zip(outcomes, methods)]
+
 
     def update_rating(self, timestamp, opponents, outcomes, methods):
         scores = Fighter.get_scores(outcomes, methods)
@@ -27,6 +32,7 @@ class Fighter(Player):
         self._update_streak(outcomes)
         self._update_history(timestamp)
     
+
     def _update_streak(self, outcomes):
         for outcome in outcomes:
             if outcome == 'win':
@@ -37,19 +43,25 @@ class Fighter(Player):
             elif outcome == 'draw':
                 continue
 
+
     def _update_history(self, timestamp):
         lower, upper = self.get_rating_interval()
         self.history.append({'timestamp': timestamp, 'rating': self.rating, 'lower': lower, 'upper': upper})
 
 
-class FighterManager(dict):
-    def __init__(self, names=[], volatility=0.3001, tau=1.86):
-        self._volatility = volatility
-        self._tau = tau
-        super().__init__({name: Fighter(volatility=self._volatility, tau=self._tau) for name in names})
 
-    def add_fighters(self, names):
-        self.update({name: Fighter(volatility=self._volatility, tau=self._tau) for name in names if name not in self})
+class FighterManager(PlayerManager):
+
+    def __init__(self, names=[], fighters=None, volatility=0.3001, tau=1.86):
+        super().__init__(ids=names, players=fighters, volatility=volatility, tau=tau)
+
+
+    def add_fighters(self, names, fighters=None):
+        if fighters is None:
+            fighters = [Fighter(volatility=self._volatility, tau=self._tau) for name in names]
+        super().add_players(ids=names, players=fighters)
+    add_players = add_fighters
+
 
     def update_fighters(self, timestamp, fights_df):
         # mirror fights_df, flipping 'outcome' so there are two rows for each fight (one for each fighter)
@@ -59,15 +71,15 @@ class FighterManager(dict):
         fights_df = pd.concat([fights_df, mirrored_df], ignore_index=True)
 
         fights_grouped = fights_df.groupby('fighter')
-        fighter_names = fights_grouped.groups.keys()
-        self.add_fighters(fighter_names)
-        fighters_copy = {name: self[name] for name in fighter_names}
+        competitor_names = fights_grouped.groups.keys()
+        self.add_fighters(competitor_names)
+        competitors_copy = {name: self[name] for name in competitor_names}
         for name, fighter in self.items():
-            if name not in fighter_names:
+            if name not in competitor_names:
                 fighter.did_not_compete()
             else:
                 fights = fights_grouped.get_group(name)
-                opponents = [fighters_copy[opponent] for opponent in fights['opponent']]
+                opponents = [competitors_copy[opponent] for opponent in fights['opponent']]
                 outcomes = fights['outcome'].tolist()
                 methods = fights['method'].tolist()
                 fighter.update_rating(timestamp, opponents, outcomes, methods)
@@ -75,3 +87,12 @@ class FighterManager(dict):
                 weight_class = fights.iloc[-1]['weight_class']
                 if weight_class != 'Catch Weight' and fighter.weight_class != weight_class:
                     fighter.weight_class = weight_class
+    update_players = update_fighters
+
+
+    def p_a_beats_b(self, name_a, name_b):
+        return super().p_a_beats_b(id_a=name_a, id_b=name_b)
+
+
+    def get_matchup_matrix(self, names=None):
+        return super().get_matchup_matrix(ids=names)
